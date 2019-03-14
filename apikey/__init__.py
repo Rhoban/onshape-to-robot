12cbf24920d2b0202155542c311bad23a79bf09d
Gregwar <g.passault@gmail.com>
2019-03-14 16:44:29 +0100

    wip

---
+++ apikey/__init__.py
@@ -0,0 +1,11 @@
+'''
+apikey
+======
+
+Demonstrates usage of API keys for the Onshape REST API
+'''
+
+__copyright__ = 'Copyright (c) 2016 Onshape, Inc.'
+__license__ = 'All rights reserved.'
+__title__ = 'apikey'
+__all__ = ['onshape', 'client', 'utils']
