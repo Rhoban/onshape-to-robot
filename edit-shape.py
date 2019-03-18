68b224038849800171d3e2d3b3ed89f2f98ba9de
Gregwar <g.passault@gmail.com>
2019-03-18 19:02:05 +0100

    Edit shape script, executable files

---
+++ edit-shape.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import sys, os
+
+if len(sys.argv) < 2:
+    print('Usage: edit-shape.py [stl file]')
+else:
+    fileName = sys.argv[1]
+    parts = fileName.split('.')
+    parts[-1] = 'scad'
+    fileName = '.'.join(parts)
+    if not os.path.exists(fileName):
+        scad = "% scale(1000) import(\""+os.path.basename(sys.argv[1])+"\");\n"
+        scad += "\n"
+        scad += "// Append pure shapes (cube, cylinder and sphere), e.g:\n"
+        scad += "// cube([10, 10, 10], center=true);\n"
+        scad += "// cylinder(r=10, h=10, center=true);\n"
+        scad += "// sphere(10);\n"
+        f = open(fileName, 'w')
+        f.write(scad)
+        f.close()
+    directory = os.path.dirname(fileName)
+    os.system('cd '+directory+'; openscad '+os.path.basename(fileName))
+
+    
--- urdf.py
+++ urdf.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import numpy as np
 from onshape_api.client import Client
 from copy import copy
