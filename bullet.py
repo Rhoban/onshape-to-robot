a217fc3b5d32b8dc72ecf7f824029d9786d2a351
Gregwar <g.passault@gmail.com>
2019-04-23 11:53:52 +0200

    Changing default simulation code to have joint sliders

--- bullet.py
+++ bullet.py
@@ -2,109 +2,24 @@
 import math
 import sys
 import pybullet as p
-from time import sleep
+from simulation import Simulation
 
 directory = 'robots/demo/'
 if len(sys.argv) > 1:
     directory = sys.argv[1]
 
-def drawInertiaBox(parentUid, parentLinkIndex, color):
-	dyn = p.getDynamicsInfo(parentUid, parentLinkIndex)
-	mass=dyn[0]
-	frictionCoeff=dyn[1]
-	inertia = dyn[2]
-	if (mass>0):
-		Ixx = inertia[0]
-		Iyy = inertia[1]
-		Izz = inertia[2]
-		boxScaleX = 0.5*math.sqrt(6*(Izz + Iyy - Ixx) / mass);
-		boxScaleY = 0.5*math.sqrt(6*(Izz + Ixx - Iyy) / mass);
-		boxScaleZ = 0.5*math.sqrt(6*(Ixx + Iyy - Izz) / mass);
-  
-		halfExtents = [boxScaleX,boxScaleY,boxScaleZ]
-		pts = [[halfExtents[0],halfExtents[1],halfExtents[2]],
-				 [-halfExtents[0],halfExtents[1],halfExtents[2]],
-				 [halfExtents[0],-halfExtents[1],halfExtents[2]],
-				 [-halfExtents[0],-halfExtents[1],halfExtents[2]],
-				 [halfExtents[0],halfExtents[1],-halfExtents[2]],
-				 [-halfExtents[0],halfExtents[1],-halfExtents[2]],
-				 [halfExtents[0],-halfExtents[1],-halfExtents[2]],
-				 [-halfExtents[0],-halfExtents[1],-halfExtents[2]]]
-	
-		
-		p.addUserDebugLine(pts[0],pts[1],color,1, parentObjectUniqueId=parentUid, parentLinkIndex = parentLinkIndex)
-		p.addUserDebugLine(pts[1],pts[3],color,1, parentObjectUniqueId=parentUid, parentLinkIndex = parentLinkIndex)
-		p.addUserDebugLine(pts[3],pts[2],color,1, parentObjectUniqueId=parentUid, parentLinkIndex = parentLinkIndex)
-		p.addUserDebugLine(pts[2],pts[0],color,1, parentObjectUniqueId=parentUid, parentLinkIndex = parentLinkIndex)
-	
-		p.addUserDebugLine(pts[0],pts[4],color,1, parentObjectUniqueId=parentUid, parentLinkIndex = parentLinkIndex)
-		p.addUserDebugLine(pts[1],pts[5],color,1, parentObjectUniqueId=parentUid, parentLinkIndex = parentLinkIndex)
-		p.addUserDebugLine(pts[2],pts[6],color,1, parentObjectUniqueId=parentUid, parentLinkIndex = parentLinkIndex)
-		p.addUserDebugLine(pts[3],pts[7],color,1, parentObjectUniqueId=parentUid, parentLinkIndex = parentLinkIndex)
-	
-		p.addUserDebugLine(pts[4+0],pts[4+1],color,1, parentObjectUniqueId=parentUid, parentLinkIndex = parentLinkIndex)
-		p.addUserDebugLine(pts[4+1],pts[4+3],color,1, parentObjectUniqueId=parentUid, parentLinkIndex = parentLinkIndex)
-		p.addUserDebugLine(pts[4+3],pts[4+2],color,1, parentObjectUniqueId=parentUid, parentLinkIndex = parentLinkIndex)
-		p.addUserDebugLine(pts[4+2],pts[4+0],color,1, parentObjectUniqueId=parentUid, parentLinkIndex = parentLinkIndex)
+sim = Simulation(directory)
 
-
-# Instanciation de Bullet
-physicsClient = p.connect(p.GUI)
-p.setGravity(0, 0, -10)
-
-# Chargement du sol
-planeId = p.loadURDF('bullet/plane.urdf')
-
-# Chargement du robot
-cubeStartPos = [0, 0, 0.1]
-cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
-robot = p.loadURDF(directory+"/robot.urdf",
-                       cubeStartPos, cubeStartOrientation)
-cubePos, cubeOrn = p.getBasePositionAndOrientation(robot)
-nJoints = p.getNumJoints(robot)
-
-# Drawing inertia boxes
-if False:
-    drawInertiaBox(robot, -1, [0,1,0])
-    for i in range (nJoints):
-        drawInertiaBox(robot, i, [0,1,0])
-
-# Map des joints
-jointsMap = []
-framesMap = []
-t = 0
-dt = 0.01
-p.setPhysicsEngineParameter(fixedTimeStep=dt)
-
-# Collecting the available joints
-for k in range (nJoints):
-    jointInfo = p.getJointInfo(robot, k)
-    name = jointInfo[1].decode('utf-8')
-    if '_fixing' not in name:
-        if '_frame' in name:
-            framesMap.append([name, k])
-        else:
-            jointsMap.append(k)
-
-print('* Found '+str(len(jointsMap))+' DOFs')
-print('* Found '+str(len(framesMap))+' frames')
+controls = {}
+for name in sim.getJoints():
+    controls[name] = p.addUserDebugParameter(name, -math.pi, math.pi, 0)
 
 while True:
-    t += dt
-    
-    for joint in jointsMap:
-        jointInfo = p.getJointInfo(robot, joint)
-        p.setJointMotorControl2(
-            robot, jointInfo[0], p.POSITION_CONTROL, math.sin(t))
+    targets = {}
+    for name in controls.keys():
+        targets[name] = p.readUserDebugParameter(controls[name])
+    sim.setJoints(targets)
 
     print('~')
-    for name, joint in framesMap:
-        print('Frame '+name)
-        jointState = p.getLinkState(robot, joint)
-        pos = jointState[0]
-        orientation = p.getEulerFromQuaternion(jointState[1])
-        print(pos)
-        print(orientation)
-        
-    p.stepSimulation()
-    sleep(dt)
+    print(sim.getFrames())
+
---
+++ simulation.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+import math
+import sys
+import time
+import pybullet as p
+from time import sleep
+
+class Simulation:
+    def __init__(self, directory):
+        self.t = 0
+        self.dt = 0.001
+
+        # Instanciation de Bullet
+        physicsClient = p.connect(p.GUI)
+        p.setGravity(0, 0, -10)
+
+        # Chargement du sol
+        planeId = p.loadURDF('bullet/plane.urdf')
+
+        # Chargement du robot
+        cubeStartPos = [0, 0, 0.75]
+        cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
+        self.robot = p.loadURDF(directory+"/robot.urdf",
+                            cubeStartPos, cubeStartOrientation)
+
+        # Engine parameters
+        p.setPhysicsEngineParameter(fixedTimeStep=self.dt)
+        p.setRealTimeSimulation(1)
+        # p.setPhysicsEngineParameter(numSolverIterations=5)
+        # p.setPhysicsEngineParameter(numSubSteps=1)
+
+        # Retrieving joints and frames
+        self.joints = {}
+        self.frames = {}
+
+        # Collecting the available joints
+        for k in range(p.getNumJoints(self.robot)):
+            jointInfo = p.getJointInfo(self.robot, k)
+            name = jointInfo[1].decode('utf-8')
+            if '_fixing' not in name:
+                if '_frame' in name:
+                    self.frames[name] = k
+                else:
+                    self.joints[name] = k
+
+        print('* Found '+str(len(self.joints))+' DOFs')
+        print('* Found '+str(len(self.frames))+' frames')
+
+    def getFrames(self):
+        frames = {}
+
+        for name in self.frames.keys():
+            jointState = p.getLinkState(self.robot, self.frames[name])
+            pos = jointState[0]
+            orientation = p.getEulerFromQuaternion(jointState[1])
+            frames[name] = [pos, orientation]
+
+        return frames
+
+    def setJoints(self, joints):
+        for name in joints.keys():
+            if name in self.joints:
+                p.setJointMotorControl2(
+                    self.robot, self.joints[name], p.POSITION_CONTROL, joints[name])
+            else:
+                raise Exception("Can't find joint %s" % name)
+
+    def getJoints(self):
+        return self.joints.keys()
+
+    def execute(self):
+        while True:
+            self.tick()
+
+    def tick(self):
+        self.t += self.dt
+        p.stepSimulation()
+
