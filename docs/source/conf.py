69ffb39f0d1aa1bf057093455e436572100f0e5b
Gregwar <g.passault@gmail.com>
2020-04-30 19:41:37 +0200

    Renaming contents to index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,4 +54,4 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-master_doc = 'contents'
+master_doc = 'index'
