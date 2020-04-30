374a287bb49f0665a3b5930b1badc947070243b5
Gregwar <g.passault@gmail.com>
2020-04-30 17:43:37 +0200

    v0.1.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README-pypi.md", "r") as fh:
 
 setuptools.setup(
     name="onshape-to-robot",
-    version="0.1.8",
+    version="0.1.9",
     author="Rhoban team",
     author_email="team@rhoban.com",
     description="Converting OnShape assembly to robot definition (SDF or URDF) through OnShape API ",
