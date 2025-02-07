e915a3f6f6749ae7feffe4646b1756d2fce4f10d
Grégoire Passault <g.passault@gmail.com>
2025-02-07 14:57:38 +0100

    Fixing escape_url method name

--- onshape_to_robot/onshape_api/client.py
+++ onshape_to_robot/onshape_api/client.py
@@ -356,6 +356,6 @@ class Client():
     
     def standard_cont_mass_properties(self, did, vid, eid, partid, linkDocumentId, configuration):
         def invoke():
-            return self._api.request('get', '/api/parts/d/' + did + '/v/' + vid + '/e/' + eid + '/partid/'+escape_slash(partid)+'/massproperties', query={'configuration': configuration, 'useMassPropertyOverrides': True, "linkDocumentId": linkDocumentId, "inferMetadataOwner": True})
+            return self._api.request('get', '/api/parts/d/' + did + '/v/' + vid + '/e/' + eid + '/partid/'+escape_url(partid)+'/massproperties', query={'configuration': configuration, 'useMassPropertyOverrides': True, "linkDocumentId": linkDocumentId, "inferMetadataOwner": True})
 
         return json.loads(self.cache_get('standard_massproperties', (did, vid, eid, self.hash_partid(partid), configuration), invoke, True))
