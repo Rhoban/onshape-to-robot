bdd9a22a5c8c296b3cc3e0dc4b7d0d7e542de90b
Grégoire Passault <g.passault@gmail.com>
2025-02-19 20:37:01 +0100

    Revert "Changing the way snake+caml conversion works"
    This reverts commit 4cb329612303bf32d6ec538c3fb0c046ab278d42.

--- onshape_to_robot/config.py
+++ onshape_to_robot/config.py
@@ -25,21 +25,12 @@ class Config:
         except OSError:
             pass
 
-    def to_snake_case(self, name: str) -> str:
+    def to_camel_case(self, snake_str: str) -> str:
         """
-        Convert configuration string to snake case for backward compatibility
+        Converts a string to camel case
         """
-        name = name.replace("STLS", "Stls")
-        name = name.replace("STL", "Stl")
-        name = name.replace("URDFS", "Urdfs")
-
-        new_name = ""
-        for i, c in enumerate(name):
-            if c.isupper() and i > 0:
-                new_name += "_"
-            new_name += c.lower()
-
-        return new_name
+        components = snake_str.split("_")
+        return components[0] + "".join(x.title() for x in components[1:])
 
     def get(self, name: str, default=None, required: bool = True, values_list=None):
         """
@@ -51,10 +42,13 @@ class Config:
             required (bool, optional): whether the configuration entry is required. Defaults to False.
             values_list: list of allowed values. Defaults to None.
         """
-        name = self.to_snake_case(name)
+        camel_name = self.to_camel_case(name)
 
-        if name in self.config:
-            value = self.config[name]
+        if name in self.config or camel_name in self.config:
+            if name in self.config:
+                value = self.config[name]
+            else:
+                value = self.config[camel_name]
 
             if values_list is not None and value not in values_list:
                 raise Exception(
