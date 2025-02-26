f70b4c00bb6e9f3c0b8399f478f1b9ef3df08054
Grégoire Passault <g.passault@gmail.com>
2025-02-26 15:19:39 +0100

    No collision meshes should be processed before collision as visual

--- onshape_to_robot/processors.py
+++ onshape_to_robot/processors.py
@@ -14,6 +14,6 @@ processors = [
     ProcessorFixedLinks,
     ProcessorDummyBaseLink,
     ProcessorConvexDecomposition,
-    ProcessorCollisionAsVisual,
     ProcessorNoCollisionMeshes,
+    ProcessorCollisionAsVisual,
 ]
