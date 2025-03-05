a97b8c7b73f87cc60719c53bb166f99469b2fd98
Grégoire Passault <g.passault@gmail.com>
2025-03-05 16:46:08 +0100

    Allowing wildcards in use fixed links

--- onshape_to_robot/processor_fixed_links.py
+++ onshape_to_robot/processor_fixed_links.py
@@ -2,6 +2,7 @@ from .processor import Processor
 from .config import Config
 from .robot import Robot, Link, Joint
 from .message import info
+import fnmatch
 
 
 class ProcessorFixedLinks(Processor):
@@ -16,15 +17,21 @@ class ProcessorFixedLinks(Processor):
         # Check if it is enabled in configuration
         self.use_fixed_links: bool | list = config.get("use_fixed_links", False)
 
+    def should_fix_links(self, link_name: str) -> bool:
+        if self.use_fixed_links == True:
+            return True
+        elif isinstance(self.use_fixed_links, list):
+            for entry in self.use_fixed_links:
+                if fnmatch.fnmatch(link_name, entry):
+                    return True
+        return False
+
     def process(self, robot: Robot):
         if self.use_fixed_links:
             print(info(f"Using fixed links ({self.use_fixed_links})"))
             new_links = []
             for link in robot.links:
-                if (
-                    isinstance(self.use_fixed_links, bool)
-                    or link.name in self.use_fixed_links
-                ):
+                if self.should_fix_links(link.name):
                     for part in link.parts:
                         part_link = Link(f"{link.name}_{part.name}")
                         part_link.parts = [part]
