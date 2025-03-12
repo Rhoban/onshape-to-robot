05223632052939b578458e5b96adf0894e42d48c
Grégoire Passault <g.passault@gmail.com>
2025-03-12 10:45:25 +0100

    Removing empty lines in XML before pretty printing

--- onshape_to_robot/exporter.py
+++ onshape_to_robot/exporter.py
@@ -15,11 +15,26 @@ class Exporter:
     def get_xml(self, robot: Robot) -> str:
         self.build(robot)
         return self.xml
+    
+    def remove_empty_text_nodes(self, node: xml.dom.Node):
+        to_delete = []
+
+        for child_node in node.childNodes:
+            if isinstance(child_node, xml.dom.minidom.Text):
+                child_node.data = child_node.data.strip()
+                if child_node.data == "":
+                    to_delete.append(child_node)
+            else:
+                self.remove_empty_text_nodes(child_node)
+
+        for child_node in to_delete:
+            node.childNodes.remove(child_node)
 
     def write_xml(self, robot: Robot, filename: str) -> str:
         with open(filename, "w") as file:
             self.build(robot)
             dom = xml.dom.minidom.parseString(self.xml)
+            self.remove_empty_text_nodes(dom)
             xml_output = dom.toprettyxml(indent="  ")
             file.write(xml_output)
             print(success(f"* Writing {os.path.basename(filename)}"))
