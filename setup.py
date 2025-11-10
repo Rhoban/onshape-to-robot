3c932cf3779fe950ed96150cdb5c24065e662137
Grégoire Passault <g.passault@gmail.com>
2025-11-10 21:26:50 +0100

    v1.7.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README-pypi.md", "r", encoding="utf-8") as stream:
 
 setuptools.setup(
     name="onshape_to_robot",
-    version="1.7.7",
+    version="1.7.8",
     author="Rhoban team",
     author_email="team@rhoban.com",
     description="Converting Onshape assembly to robot definition (URDF, SDF, MuJoCo) through Onshape API ",
